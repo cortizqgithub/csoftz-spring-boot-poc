66ec8c7c1e67888fc935e59ba7ca6dc06bc1a11f
Carlos Adolfo Ortiz Quiros <carlosaortiz@yahoo.com>
2019-03-22 17:08:02 -0500

    Includes an assert because to comply with Sonar rule 'Tests should include assertions'

--- app-code/devops/src/test/java/com/example/devops/SpringBootPOCTests.java
+++ app-code/devops/src/test/java/com/example/devops/SpringBootPOCTests.java
@@ -3,7 +3,7 @@
 /* Description:   Spring Boot Context test.                                   */
 /* Author:        Carlos Adolfo Ortiz Quirós (COQ)                            */
 /* Date:          Mar.01/2019                                                 */
-/* Last Modified: Mar.16/2019                                                 */
+/* Last Modified: Mar.22/2019                                                 */
 /* Version:       1.1                                                         */
 /* Copyright (c), 2019 CSoftZ                                                 */
 /*----------------------------------------------------------------------------*/
@@ -13,26 +13,33 @@
  -----------------------------------------------------------------------------*/
 package com.example.devops;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * Spring Boot Context test.
  *
  * @author Carlos Adolfo Ortiz Quirós (COQ)
- * @version 1.1, Mar.16/2019
+ * @version 1.1, Mar.22/2019
  * @since 1.8 (JDK), Mar.01/2019
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootPOCTests {
+    @Autowired
+    private ApplicationContext appContext;
 
     /**
      * Load Spring Boot App to validate it is well configured.
      */
     @Test
     public void contextLoads() {
+        assertThat(appContext).isNotNull();
     }
 }
