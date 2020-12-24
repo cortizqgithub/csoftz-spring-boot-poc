cd4a1aac675553a2f055633c2f1cee385d1eaa50
Carlos Adolfo Ortiz Quiros <carlosaortiz@yahoo.com>
2020-12-24 10:52:23 -0500

    Add CZ-EXAMPLES project to include any POC in there.

---
+++ app-code/cz-examples/src/main/java/com/example/cz/examples/CzExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.example.cz.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CzExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CzExamplesApplication.class, args);
+	}
+
+}
