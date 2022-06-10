126ddf202c63983c96438c500c1ca57a7812269c
Carlos Adolfo Ortiz Quiros <carlosaortiz@yahoo.com>
2022-06-10 14:57:26 -0500

    Update maven wrapper. Add spring-boot-openrewrite to play with OpenRewrite

---
+++ app-code/spring-boot-openrewrite/src/main/java/com/csoftz/openrewrite/SpringBootOpenrewriteApplication.java
@@ -0,0 +1,13 @@
+package com.csoftz.openrewrite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOpenrewriteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOpenrewriteApplication.class, args);
+    }
+
+}
---
+++ app-code/spring-boot-openrewrite/src/test/java/com/csoftz/openrewrite/SpringBootOpenrewriteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csoftz.openrewrite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOpenrewriteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
